fe677881ddb314fddace75d0153249beedb8ee73
Jacob-Gruver <gruvinbro@gmail.com>
2022-03-31 16:56:49 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -25,19 +25,32 @@ export default {
 }
 </script>
 
-<style lang>
+<style>
 
-/* #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
 }
 
 main#router-view-slot{
   background: green;
   flex-grow: 1;
-} */
+}
+/* #app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: $body-bg;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+  */
 </style>
