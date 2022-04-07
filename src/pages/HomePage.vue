b17cf6c5b537e842f399b92b715fc284d793309a
Jacob-Gruver <gruvinbro@gmail.com>
2022-04-07 15:37:50 -0600

    init

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,11 +9,8 @@
       justify-content-center
     "
   >
-    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Home-Page</span>
-      <img class="my-5" src="../assets/img/autobot.png" />
-      <img class="my-5" src="../assets/img/decepticon.png" />
-    </h1>
+    <img class="my-5" src="../assets/img/autobot.png" />
+    <img class="my-5" src="../assets/img/decepticon.png" />
   </div>
 </template>
 <script>
